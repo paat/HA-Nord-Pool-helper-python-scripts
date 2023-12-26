bc6d9b4f79906c27de94883ef90142e2ee9fb924
tchernikov@gmail.com <tchernikov@gmail.com>
2023-12-26 09:34:10 +0200

    Shell script for automated copy of script files - fix

--- update_and_copy.sh
+++ update_and_copy.sh
@@ -7,4 +7,4 @@
 git pull
 
 # Copy .py files from python_scripts to the parent directory, excluding __init__.py
-find python_scripts -maxdepth 1 -name "*.py" ! -name "__init__.py" -exec cp {} . \;
+find python_scripts -maxdepth 1 -name "*.py" ! -name "__init__.py" -exec cp {} .. \;
