32e2d9a1a9399b8921775418cb9ac331f7637a8c
tchernikov@gmail.com <tchernikov@gmail.com>
2023-12-26 19:40:50 +0200

    Improve algorithm - look only in surrounding 24-hour window if array of prices is for 2 days

--- python_scripts/set_low_price_status.py
+++ python_scripts/set_low_price_status.py
@@ -1,19 +1,27 @@
 # set_low_price_status.py in <config>/python_scripts/
-def device_should_be_on(prices_by_h, price_current, price_low, hours):
+
+def device_should_be_on24(prices_by_h, price_current, price_low, hours, current_hour):
     """
-    :param prices_by_h: prices ordrered by hour today+tomorrow
+    :param hours: number of hours device should be on
+    :param prices_by_h: prices ordered by hour today+tomorrow
     :param price_current: current nordpool price
-    :param price_low: low threshold (price when it is not point to optimize)
+    :param price_low: low threshold (price when it is no point to optimize)
     :return: true if device should be turned on or false
     """
     if len(prices_by_h) <= hours:
         return True
-    prices_sorted = sorted(prices_by_h)
+
+    if len(prices_by_h) <= 24:
+        prices_by_h_24 = prices_by_h
+    else:
+        # look in the scope of 24-hour window
+        prices_by_h_24 = prices_by_h[max(0, current_hour - 12):min(current_hour + 12, len(prices_by_h) - 1)]
+
+    prices_sorted = sorted(prices_by_h_24)
     prices_lowest = prices_sorted[:hours]
-    if price_current in prices_lowest or price_current < price_low:
+    if price_current <= prices_lowest[-1] or price_current < price_low:
         return True
     return False
-
 
 # home assistant start executing here
 try:
@@ -41,6 +49,9 @@ if data and hass:
         prices_by_hour = today_prices + tomorrow_prices if tomorrow_prices else today_prices
 
         current_price = float(sensor_state.attributes.get('current_price'))
-        result = device_should_be_on(prices_by_hour, current_price, low_price, number_of_hours)
-        hass.states.set(input_boolean_id, 'on' if result else 'off')
 
+        now = datetime.now()
+        current_hour = now.hour
+
+        result = device_should_be_on24(prices_by_hour, current_price, low_price, number_of_hours, current_hour)
+        hass.states.set(input_boolean_id, 'on' if result else 'off')
--- tests/set_low_price_status_tests.py
+++ tests/set_low_price_status_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-from python_scripts.set_low_price_status import device_should_be_on
+from python_scripts.set_low_price_status import device_should_be_on24
 
 
 class TestDeviceShouldBeOn(unittest.TestCase):
@@ -10,7 +10,8 @@ class TestDeviceShouldBeOn(unittest.TestCase):
         price_current = 2.0
         price_low = 1.5
         number_of_hours = 5
-        self.assertTrue(device_should_be_on(prices_by_h, price_current, price_low, number_of_hours),
+        current_hour = 19
+        self.assertTrue(device_should_be_on24(prices_by_h, price_current, price_low, number_of_hours, current_hour),
                         "Device should be on when the current price is among the lowest prices.")
 
     def test_device_on_below_low_price(self):
@@ -18,7 +19,8 @@ class TestDeviceShouldBeOn(unittest.TestCase):
         price_current = 1.0
         price_low = 1.5
         number_of_hours = 5
-        self.assertTrue(device_should_be_on(prices_by_h, price_current, price_low, number_of_hours),
+        current_hour = 19
+        self.assertTrue(device_should_be_on24(prices_by_h, price_current, price_low, number_of_hours, current_hour),
                         "Device should be on when the current price is below the low price threshold.")
 
     def test_device_off_not_in_lowest_prices(self):
@@ -26,9 +28,23 @@ class TestDeviceShouldBeOn(unittest.TestCase):
         price_current = 6.0
         price_low = 1.5
         number_of_hours = 5
-        self.assertFalse(device_should_be_on(prices_by_h, price_current, price_low, number_of_hours),
+        current_hour = 19
+        self.assertFalse(device_should_be_on24(prices_by_h, price_current, price_low, number_of_hours, current_hour),
                          "Device should be off when the current price is not among the lowest prices and above the "
                          "low price threshold.")
+
+    def test_device_on_24hour_window_lowest_price(self):
+        prices_by_h = [0.027, 3.676, 3.368, 3.275, 3.416, 3.72, 4.022, 4.062, 4.398, 4.685,
+                       5.947, 6.023, 5.995, 5.905, 5.462, 5.291, 5.142, 5.695, 6.346, 6.354,
+                       5.93, 5.044, 4.891, 4.549, 4.08, 4.213, 3.732, 3.682, 3.718, 3.906,
+                       4.714, 7.807, 8.294, 9.326, 9.342, 8.807, 8.705, 8.642, 8.49, 8.792,
+                       9.524, 10.116, 10.042, 9.61, 8.996, 6.568, 4.8, 0.998]
+        price_current = 3.0
+        price_low = 1.5
+        number_of_hours = 1
+        current_hour = 19
+        self.assertTrue(device_should_be_on24(prices_by_h, price_current, price_low, number_of_hours, current_hour),
+                         "Device should be on when the current price is not among the lowest prices within 24-hour window.")
 
 
 if __name__ == '__main__':
